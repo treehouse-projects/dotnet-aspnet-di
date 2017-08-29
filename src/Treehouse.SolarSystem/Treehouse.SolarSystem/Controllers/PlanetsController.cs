399465d3f6970f4b3d7bdb5a3fd2a415fd06bf70
James Churchill <james@smashdev.com>
2017-08-28 21:49:29 -0700

    First pass at the MVC example project

---
+++ src/Treehouse.SolarSystem/Treehouse.SolarSystem/Controllers/PlanetsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.SolarSystem.Data;
+
+namespace Treehouse.SolarSystem.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        private Repository _repository = null;
+
+        public PlanetsController()
+        {
+            var context = new Context();
+            _repository = new Repository(context);
+        }
+
+        public ActionResult Index()
+        {
+            var planets = _repository.GetPlanets();
+
+            return View(planets);
+        }
+    }
+}
