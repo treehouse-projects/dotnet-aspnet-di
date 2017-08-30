90e2d2818e79bebca60c3fae2c0e6b6e7a7a5244
James Churchill <james@smashdev.com>
2017-08-30 11:13:22 -0700

    Added the ASP.NET Core project

---
+++ src/Treehouse.SolarSystem/Treehouse.AspNetCore/Services/ITestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Treehouse.AspNetCore.Services
+{
+    public interface ITestService
+    {
+        string GetAboutContent();
+    }
+}
